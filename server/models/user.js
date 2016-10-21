b149907689d3e0f9359de82c7a31279a8029ca46
Diego Vieira <DiegoVieira@Diegos-MacBook-Pro.local>
2016-10-21 20:14:35 -0200

    Added POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -90,6 +90,26 @@ UserSchema.statics.findByToken = function (token) {
     });
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({ email }).then(user => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, success) => {
+                if (success) {
+                    return resolve(user);
+                }
+
+                reject(err);
+            });
+        });
+    });
+}
+
 // MIDDLEWARES
 UserSchema.pre('save', function (next) {
     var user = this;
