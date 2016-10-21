ecc9553ce5b2c91b6917a953167eb3deb22ffded
Diego Vieira <DiegoVieira@Diegos-MacBook-Pro.local>
2016-10-20 22:58:54 -0200

    Added GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -32,7 +32,13 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
-// this overrides the built in toJSON method. 
+// THE METHODS BELLOW ARE, INSTANCE METHODS
+// THEY CAN BE ACCESSED ONLY BY AN INSTANTIATED USER
+
+// this overrides the built in toJSON methods.
+// overriding toJSON, the response sent by express,
+// will only contains the pick fields (security reasons).
+// the client don't need to know password and tokens
 UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
@@ -41,6 +47,7 @@ UserSchema.methods.toJSON = function () {
 }
 
 // not using lambda, because on lambdas [this] keyword isn't valid 
+// this function will be used when a new user is created
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
@@ -51,6 +58,33 @@ UserSchema.methods.generateAuthToken = function () {
     // returning a promise, for chaining
     return user.save().then(() => {
         return token;
+    });
+}
+
+// THE METHODS BELLOW ARE MODEL METHODS, THEY CAN BE
+// ACCESSED FROM THE USER SCHEMA, WITHOUT AN INSTANCE.
+// THEY ARE STATIC, LIKE IN C#
+
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        // return new Promise((resolve, reject) => {
+        //     reject();
+        // });
+
+        // same as above
+        return Promise.reject();
+    }
+
+    // returning a promise, for chaining
+    return User.findOne({
+        _id: decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
     });
 }
 
