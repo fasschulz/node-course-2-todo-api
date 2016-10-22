c01c875625d5f9f52f92601b1e922b99eb10cc53
Diego Vieira <DiegoVieira@Diegos-MacBook-Pro.local>
2016-10-22 15:53:37 -0200

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,6 @@
 var env = process.env.NODE_ENV || 'development';
+env = env.trim();
+
 console.log(`Running on environment: ${env}`);
 
 if (env === 'development' || env === 'test') {
@@ -6,6 +8,7 @@ if (env === 'development' || env === 'test') {
     var envConfig = config[env];
 
     Object.keys(envConfig).forEach((key) => {
-        prompt.env[key] = envConfig[key];
+        console.log('Setting:', key);
+        process.env[key] = envConfig[key];
     });
 }
